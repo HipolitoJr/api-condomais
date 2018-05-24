c6f5ba9b7cb651b91090c9952a0adac1a53e59be
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-05-24 20:54:14 -0300

    Initial commit

---
+++ condomais/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for condomais project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "condomais.settings")
+
+application = get_wsgi_application()

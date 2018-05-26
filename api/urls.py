ce6ec579791b543289f0c25ab369204d1190ecc6
RodrigoAzuos <rodrill.068@gmail.com>
2018-05-26 07:35:08 -0300

    feita correção de erros de gráfia nas urls.

--- api/urls.py
+++ api/urls.py
@@ -5,10 +5,10 @@ from . import views
 router = DefaultRouter()
 
 router.register(r'proprietario', views.ProprietarioViewSet)
-router.register(r'unidade-habitarional', views.UnidadeHabitacionalViewSet)
+router.register(r'unidade-habitacional', views.UnidadeHabitacionalViewSet)
 router.register(r'grupo-habitacional', views.GrupoHabitacionalViewSet)
 router.register(r'condominio', views.CondominioViewSet)
-router.register(r'taxa-condomio', views.TaxaCondominioViewSet)
+router.register(r'taxa-condominio', views.TaxaCondominioViewSet)
 router.register(r'item-taxa', views.ItemTaxaViewSet)
 router.register(r'despesa', views.DespesaViewSet)
 router.register(r'tipo-despesa', views.TipoDespesaViewSet)

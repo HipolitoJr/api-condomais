30313c12e4b8558244d384a74e08f7963ac642dc
vinilopes1 <lopesvinicius1707@gmail.com>
2018-05-25 22:10:51 -0300

    Adicionando a viewset "ProprietarioViewSet".

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from api.models import Proprietario, UnidadeHabitacional, GrupoHabitacional, Condominio, TaxaCondominio, ItemTaxa, Despesa, TipoDespesa
+from api.serializers import ProprietarioSerializer, UnidadeHabitacionalSerializer, GrupoHabitacionalSerializer, CondominioSerializer, TaxaCondominioSerializer, ItemTaxaSerializer, DespesaSerializer, TipoDespesaSerializer
+from rest_framework import authentication, permissions, viewsets
 
-# Create your views here.
+class DefaultsMixin(object):
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
+
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+
+class ProprietarioViewSet(DefaultsMixin, viewsets.ModelViewSet):
+
+    queryset = Proprietario.objects.all()
+    serializer_class = ProprietarioSerializer
+
+

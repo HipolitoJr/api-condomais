9c32912877b2ac87971877ecb6a090f0e8ca7bf3
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-06-04 10:36:08 -0300

    configuração heroku

--- condomais/settings.py
+++ condomais/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'condomais-api.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'condomais.herokuapp.com']
 
 
 # Application definition
